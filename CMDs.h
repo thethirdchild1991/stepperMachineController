2560fc73724d7a4931040741b5eb553532c6d02c
Denis <denis.dylko@gmail.com>
2019-05-28 23:58:24 +0300

    .factory method for cmd creation

--- CMDs.h
+++ CMDs.h
@@ -5,6 +5,19 @@
 #include <QDebug>
 
 namespace CMDs{
+
+    /* CMD STRUCT
+     *  json {
+     *      CMD_CODE : CODE,
+     *      CMD_PARAM : {
+     *          param_name : name,
+     *          param_value : value
+     *      }
+     * }
+     *
+     */
+
+
     enum class CMDs_NAMES{
         MOVE_mSTEPS,    //move N microsteps
         MOVE_mm,        //move N milimeters
@@ -14,40 +27,89 @@ namespace CMDs{
         BACK_TO_ZERO    //return to zero point
     };
 
-    using CMD_PARAM_t = uint32_t;
-//    static void execute( CMDs_NAMES cmdType, CMD_PARAM_t param ){
+    using CMD_DATA_t = std::pair<int, int>;
+
+    struct CMD_DATA{
+        CMDs_NAMES CODE;
+        CMD_DATA_t DATA;
+    };
+
+
+//    static void execute( CMDs_NAMES cmdType, CMD_DATA_t param ){
 
 //    }
+    static void makeUStep(){}
+    static void makeMMStep(){}
+    static void setParameter( CMD_DATA_t param ){}
+    static CMD_DATA_t getParameter( CMD_DATA_t param ){}
+    static void setZero(){}
+    static void backToZero(){}
 
     class CMD {
         public:
+            explicit CMD( CMDs_NAMES code ) : mCode(code) {}
+            virtual ~CMD() = 0;
 
-        explicit CMD( CMDs_NAMES type ) : mType(type) {}
-        virtual ~CMD(){}
+            virtual void execute( CMD_DATA_t param ) = 0;
 
-        virtual void execute( CMD_PARAM_t param ){
-            qDebug() << "ERROR, from CMD ABSTRACT CLASS";
-        }
+            void type( CMDs_NAMES value ) { mType = value; }
+            CMDs_NAMES type() const { return mType; }
 
-        void type( CMDs_NAMES value ) { mType = value; }
-        CMDs_NAMES type() const { return mType; }
+            static CMD* createCMD( CMD_DATA& cmd );
 
 
-        private:
-        CMDs_NAMES mType;
+        protected:
+            CMDs_NAMES mCode;
+            CMD_DATA_t mData;
     };
 
-    class CMD_MOVE_mSTEPS : virtual public CMD{};
+    class CMD_MOVE{
+        public:
+            CMD_MOVE( CMD_DATA_t delta ) :  mDelta{delta} {}
+        protected:
+            CMD_DATA_t dec( CMD_DATA_t param ){
+                return param - mDelta;
+            }
+            CMD_DATA_t mDelta;
+    };
 
-    class CMD_MOVE_mm : virtual public CMD{};
+    class CMD_MOVE_mSTEPS : public CMD_MOVE, virtual public CMD{
+        public:
+            explicit CMD_MOVE_mSTEPS( CMD_DATA_t delta ) : CMD_MOVE(delta), CMD ( CMDs_NAMES::MOVE_mSTEPS ) {}
+            void execute( CMD_DATA_t param ) override{
+                while(param > 0){
+                    makeUStep();
+                    param = dec(param);
+                }
+            }
+    };
 
-    class CMD_SET : virtual public CMD{};
+    class CMD_MOVE_mm {
+    };
 
-    class CMD_GET : virtual public CMD{};
+    class CMD_SET : virtual public CMD{
+        public:
+            void execute( CMD_DATA_t param ) override{
+                setParameter( param );
+            }
+    };
 
-    class CMD_SET_ZERO : virtual public CMD{};
+    class CMD_GET : virtual public CMD{
+        public:
+            void execute( CMD_DATA_t param ) override{
+                getParameter( param );
+            }
+    };
 
-    class CMD_BACK_TO_ZERO : virtual public CMD{};
+    class CMD_SET_ZERO : virtual public CMD{
+        public:
+            void execute( CMD_DATA_t param ) override{
+
+        }
+    };
+
+    class CMD_BACK_TO_ZERO{
+    };
 
 
 };
